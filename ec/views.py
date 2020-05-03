cec7de6d448d2290f7de5d4d29fc249a2fed8949
drosophila1402 <drosophila0619@gmail.com>
2020-05-03 11:06:39 +0000

    second

--- ec/views.py
+++ ec/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'ec/index.html', {})
+    
+def test(request):
+    return render(request, 'base/test.html', {})
