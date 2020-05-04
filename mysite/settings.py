0f7305aaf1424d12366c82c12779a6b09ece2fde
drosophila1402 <drosophila0619@gmail.com>
2020-05-04 12:14:26 +0000

    changed cart layout

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '($u(s*cl34%5i844g)$*)k^oi#zkn=+)=hlnr$^y6+0r#3owyw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '37c00df061944e218fe3997cd5435c38.vfs.cloud9.us-east-2.amazonaws.com',
